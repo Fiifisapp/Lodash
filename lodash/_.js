e7fef600e44fc543b064f41e5cd12061d18cf20e
Fiifisapp <91861643+Fiifisapp@users.noreply.github.com>
2021-12-16 14:22:24 +0000

    Add files via upload

---
+++ lodash/_.js
@@ -0,0 +1,99 @@
+const _ ={
+  clamp(number, lower, upper){
+    var lowerClampedValue = Math.max(number, lower);
+    var clampedValue = Math.min(lowerClampedValue, upper);
+    return clampedValue;
+  },
+  inRange(number, start, end){
+    if (end === undefined){
+        end = start
+        start = 0
+    };
+    if (start > end){
+      var temp = end
+      end = start
+      start = temp
+    };
+    var isInRange = start <= number && number < end;
+    return isInRange
+  },
+  words(string){
+    var words = string.split(' ');
+    return words
+  },
+  pad (string, length){
+    if(string.length >= length){
+      return string;
+    };
+    var startPaddingLength = Math.floor((length - string.length) / 2);
+    var endPaddingLength = length - string.length - startPaddingLength;
+    var paddedString = " ".repeat(startPaddingLength) + string + " ".repeat(endPaddingLength);
+    return paddedString;
+  },
+  has (object, key){
+    var hasValue = object[key];
+    if (hasValue != undefined){
+      return true;
+    }
+    else{
+      return false;
+    }
+  },
+invert (object){
+var invertedObject = {};
+for (let key in object ){
+  const originalValue = object[key];
+    invertedObject = {originalValue : key};
+}
+return invertedObject;
+},
+
+findKey (object, predicate){
+ for (let key in object){
+   let value = object[key];
+   let predicateReturnValue = predicate(value);
+   if (predicateReturnValue){
+     return key;
+   }
+ }
+undefined 
+return undefined;
+},
+drop (array, n){
+  if (n === undefined){
+    n = 1;
+  }
+  var droppedArray = array.slice(n, array.length)
+  return droppedArray;
+},
+dropWhile(array, predicate){
+  const db  = (element, index) => {
+    return !predicate(element, index, array);
+  };
+var dropNumber = array.findIndex(db);
+var droppedArray = this.drop(array, dropNumber);
+return droppedArray;
+},
+chunk (array, size){
+  if (size === undefined){
+    size = 1;
+  }
+  var arrayChunks = [];
+  for (let i = 0; i < array.length; i += size){
+    var arrayChunk = array.slice(i, i+size);
+    arrayChunks.push(arrayChunk);
+  }
+  return arrayChunks;
+}
+
+
+
+
+
+};
+
+
+
+
+// Do not write or modify code below this line.
+module.exports = _;
